5c920fca7248442dbcdd88b51254044f2897e36d
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2024-04-02 14:17:11 +1300

    updated react app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,19 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { useState } from 'react';
-import Home from '@pages/Home';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { useState } from 'react';
+// import Home from '@pages/Home';
 
-const router = createBrowserRouter([
-  {
-    path: '/:name',
-    element: <Home />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/:name',
+//     element: <Home />,
+//   },
+// ]);
 
 export default function App() {
-  const [queryClient] = useState(() => new QueryClient());
   return (
     <div>
-      <h1>hello</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 }
