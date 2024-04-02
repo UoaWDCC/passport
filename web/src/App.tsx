39195552da5af62cafbb3d641db916f75198a49e
justin <justinhuang2020@gmail.com>
2024-04-02 15:41:33 +1300

    Add link import

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,17 @@
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 // import { useState } from 'react';
 // import Home from "@pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <p>Hello World!</p>,
+    element: (
+      <div>
+        <p>Hello World!</p>
+        <Link to="newpage">Other Page</Link>
+      </div>
+    ),
   },
   {
     path: "/newpage",
