2df6d6aa000f55fdb3664e53a4744a2f98be4da4
justin <justinhuang2020@gmail.com>
2024-04-02 15:34:19 +1300

    Configure routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,19 @@
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import { useState } from 'react';
-// import Home from '@pages/Home';
+// import Home from "@pages/Home";
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/:name',
-//     element: <Home />,
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Hello World!</p>,
+  },
+  {
+    path: "/newpage",
+    element: <p>Insert element here</p>,
+  },
+]);
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
