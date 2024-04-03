e41e9c96366986aa27892bc2eec52be098da9a3b
justin <jhua515@aucklanduni.ac.nz>
2024-04-03 14:41:32 +1300

    Add logo to main page and resize pages

--- web/src/components/PassportMain.tsx
+++ web/src/components/PassportMain.tsx
@@ -1,9 +1,11 @@
+import Logo from "../images/secondary_white_1024.png";
 import "../styles/Passport.css";
 
 export default function PassportMain() {
   return (
-    <div className="passport w-80 h-96 m-auto rounded-br-3xl">
-      <h1>This is the passport main page</h1>
+    <div className="passport passport-main m-auto rounded-br-3xl flex justify-center items-center">
+      {/* <h1>This is the passport main page</h1> */}
+      <img src={Logo} alt="" className="h-80" />
     </div>
   );
 }
--- web/src/components/PassportPage.tsx
+++ web/src/components/PassportPage.tsx
@@ -2,7 +2,7 @@ import "../styles/Passport.css";
 
 export default function PassportPage() {
   return (
-    <div className="bg-white w-80 h-96 m-auto rounded-br-3xl">
+    <div className="passport bg-white m-auto rounded-br-3xl">
       <h1>This is the passport page</h1>
     </div>
   );
