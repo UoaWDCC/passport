4cc0f80c9a085adfb8c332aa13123b1e54daa4e4
justin <jhua515@aucklanduni.ac.nz>
2024-04-03 13:56:16 +1300

    Add some styling

--- web/src/pages/Passport.tsx
+++ web/src/pages/Passport.tsx
@@ -1,7 +1,11 @@
+import "../styles/Passport.css";
+
 export default function Passport() {
   return (
-    <div>
-      <h1>This is the passport</h1>
+    <div className="background flex h-screen justify-center">
+      <div className="passport w-80 h-96 m-auto rounded-br-3xl">
+        <h1>This is the passport</h1>
+      </div>
     </div>
   );
 }
