da39a419799c42a97b64e9603284b44e31589970
justin <jhua515@aucklanduni.ac.nz>
2024-04-03 13:40:05 +1300

    Create new page for passport component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 // import { useState } from 'react';
 // import Home from "@pages/Home";
+import Passport from "@pages/Passport";
 
 const router = createBrowserRouter([
   {
@@ -9,13 +10,13 @@ const router = createBrowserRouter([
     element: (
       <div>
         <p>Hello World!</p>
-        <Link to="newpage">Other Page</Link>
+        <Link to="passport">Other Page</Link>
       </div>
     ),
   },
   {
-    path: "/newpage",
-    element: <p>Insert element here</p>,
+    path: "/passport",
+    element: <Passport />,
   },
 ]);
 
