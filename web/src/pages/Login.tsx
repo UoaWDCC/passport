3a8c391f995986177db468609c638aae620068b3
bwon783 <86991521+BW82262@users.noreply.github.com>
2024-04-03 21:43:55 +1300

    Added JWT Decoding and Post to DB of user data

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,11 +1,44 @@
 import { GoogleLogin } from "@react-oauth/google"
+import { jwtDecode } from "jwt-decode"
 
 function Login() {
   return (
     <div>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          console.log(credentialResponse)
+          console.log(credentialResponse.credential)
+          if (
+            credentialResponse.credential == null ||
+            credentialResponse.credential == undefined
+          ) {
+            throw new Error("Credential is null or undefined")
+          } else {
+            const jsondata = jwtDecode(credentialResponse.credential)
+            console.log(jsondata)
+
+            const postUserData = async () => {
+              await fetch("http://localhost:3000/api/v1/users", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                  firstName: jsondata!.given_name,
+                  lastName: jsondata!.family_name,
+                  email: jsondata!.email,
+                  accessToken: jsondata!.jti,
+                }),
+              })
+                .then((response) => {
+                  console.log(response)
+                })
+                .catch((error) => {
+                  console.log(error)
+                })
+            }
+
+            postUserData().then(() => {
+              localStorage.setItem("accessToken", jsondata.jti)
+            })
+          }
         }}
         onError={() => {
           console.log("Login Failed")
