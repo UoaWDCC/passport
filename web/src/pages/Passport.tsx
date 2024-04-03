a295374f893a25007bc740af4c8e7dbe7dc5b4ca
justin <jhua515@aucklanduni.ac.nz>
2024-04-03 14:14:26 +1300

    Implement page switching functionality

--- web/src/pages/Passport.tsx
+++ web/src/pages/Passport.tsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import PassportMain from "@components/PassportMain";
 import PassportPage from "@components/PassportPage";
 import "../styles/Passport.css";
 
 export default function Passport() {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const views = [PassportMain, PassportPage, PassportPage];
-  const CurrentView = views[0];
+
+  const goToNextView = () => {
+    if (currentIndex < views.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const goToPreviousView = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const CurrentView = views[currentIndex];
 
   return (
-    <div className="background flex h-screen justify-center">
+    <div className="background flex flex-col h-screen justify-center items-center ">
       <CurrentView />
+
+      <div className="text-black">
+        {currentIndex > 0 && (
+          <button onClick={goToPreviousView}>Previous</button>
+        )}
+        {currentIndex < views.length - 1 && (
+          <button onClick={goToNextView}>Next</button>
+        )}
+      </div>
+
+      <p>Page {currentIndex + 1}</p>
     </div>
   );
 }
