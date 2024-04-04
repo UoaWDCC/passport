33440985c432c4f50edb953b8a99492de6eadf6a
Nancy Wei <nnwi818@aucklanduni.ac.nz>
2024-04-05 12:18:24 +1300

    added initial version for the signin pages

---
+++ web/src/pages/SignIn-Page/SignInPage.tsx
@@ -0,0 +1,28 @@
+import styles from "./SignInPage.module.css"; // Import the CSS module
+import GoogleSigninBtn from "../../components/GoogleSigninBtn";
+
+const handleSignIn = () => {
+  // calls an endpoint to redirect to Google auth (current a place holder)
+  window.open("https://placeholder.com", "_blank");
+};
+
+const SignInPage: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.topsection}>
+        <h1 className={styles.h1}>WDCC</h1>
+        <p className={styles.subtitle}>Web Dev & Consulting Club</p>
+      </div>
+      
+      <div className={styles.bottomsection}>
+        <p className={styles.text}>Welcome to WDCC Passport!</p>
+        <p className={styles.text}>Please sign in with your Google account to proceed.</p>
+        <GoogleSigninBtn onClick={handleSignIn} /> {/* Use the custom Google sign-in button */}
+        <p className={styles.text}>Or</p>
+        <a className={styles.text} href="https://forms.gle/YX8RAdXGF4rTN3e27">Register Here</a>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
