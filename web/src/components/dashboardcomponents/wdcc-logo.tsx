c80d0fe9b6e9a99e36dd125a1bf98679b6de6f9a
CalebWharton <calebwharton123@gmail.com>
2024-04-06 22:01:52 +1300

    updated routing to include my pages

--- web/src/components/dashboardcomponents/wdcc-logo.tsx
+++
@@ -1,9 +0,0 @@
-import LogoImage from '../../assets/WDCC_Logo.svg';
-
-function Logo() {
-    return (
-        <img className='logo' src={LogoImage} alt="WDCC Logo" />
-    );
-}
-
-export default Logo;
--- web/src/pages/components/dashboardcomponents/create-new-button.tsx
+++ web/src/pages/components/dashboardcomponents/create-new-button.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import externalLink from '../../../assets/externalLink.svg';
+import externalLink from '../../../assets/externalLink.svg';
 
 function CreateNewButton() {
     return (
