241feeaa95bd63b1654cfdc6086628a35daf0702
justin <jhua515@aucklanduni.ac.nz>
2024-04-07 22:02:46 +1200

    Implement hamburger menu functionality

--- web/src/components/HamburgerMenu.tsx
+++ web/src/components/HamburgerMenu.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import "../styles/HamburgerMenu.css";
+
 export default function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="hamburger-menu relative flex mt-6">
+      <div
+        className={`hamburger-icon flex flex-col cursor-pointer ${
+          isOpen ? "open" : ""
+        }`}
+        onClick={toggleMenu}
+      >
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+
+      <div className={`menu-items ${isOpen ? "open" : ""}`}>
+        <a href="#">Passport Page</a>
+        <a href="#">Leaderboard Page</a>
+      </div>
     </div>
   );
 }
