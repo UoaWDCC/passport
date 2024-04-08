f9cf7d3c2110a88a10d7b33f48f26c26f9359e83
CalebWharton <calebwharton123@gmail.com>
2024-04-08 18:26:38 +1200

    adopted uniform file naming

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,11 +1,10 @@
-// app.tsx
-import GenerateQRCodeButton from './components/dashboardcomponents/create-qr-code-button';
-import Header from './components/dashboardcomponents/dashboard-header';
+import GenerateQRCodeButton from '../components/dashboardcomponents/create-qr-code-button';
+import Header from '../components/dashboardcomponents/dashboard-header';
 import '../styles/dashboard.css';
 
 function Dashboard() {
     return (
-        <div>
+        <div className='dashboard-container'>
             <Header />
             <GenerateQRCodeButton />
         </div>
