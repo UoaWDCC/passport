d785b56d8c3145083c5e7e978094683ad428d81c
Erica <engi850@aucklanduni.ac.nz>
2024-04-08 23:33:47 +1200

    Created react components for both buttons, and implemented onto /passport

--- web/src/pages/Passport.tsx
+++ web/src/pages/Passport.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import PassportMain from "@components/PassportMain";
 import PassportPage from "@components/PassportPage";
-import HamburgerMenu from "@components/HamburgerMenu";
+import LeftButton from "../assets/LeftButton.tsx";
+import RightButton from "../assets/RightButton.tsx";
+
 import "../styles/Passport.css";
 
 export default function Passport() {
@@ -32,17 +34,16 @@ export default function Passport() {
     <div className="background flex flex-col h-screen justify-center items-center ">
       <HamburgerMenu />
       <CurrentView />
-  
-        {/* Display navigation buttons */}
-      <div className="text-black flex justify-center gap-4">
-        {/* Display arrows depending on the current page index */}
+
+      <div className="text-black">
+        {/* display arrows depending on the current page index */}
         {currentIndex > 0 && (
-          <button onClick={goToPreviousView} className="svg-button">
+          <button onClick={goToPreviousView}>
             <LeftButton />
           </button>
         )}
         {currentIndex < views.length - 1 && (
-          <button onClick={goToNextView} className="svg-button">
+          <button onClick={goToNextView}>
             <RightButton />
           </button>
         )}
