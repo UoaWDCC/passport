c61f940e678716034c061dc0fc497cedbcf3d729
CalebWharton <calebwharton123@gmail.com>
2024-04-08 17:15:41 +1200

    fixed input sizes, removed underline from link

--- web/src/pages/components/dashboardcomponents/create-new-button.tsx
+++ web/src/pages/components/dashboardcomponents/create-new-button.tsx
@@ -3,12 +3,12 @@ import externalLink from '../../../assets/externalLink.svg';
 
 function CreateNewButton() {
     return (
-        <Link to='/form'>
+        <a href='/form'>
             <button className='create-new-button'>
                 <h3 className='create-new'>Create new</h3>
                 <img className='external-link' src={externalLink} alt="Icon" />
             </button>
-        </Link>
+        </a>
     );
 }
 
