87a117b43114aee0020cb882043e2d75bfa4d4d4
Nancy Wei <nnwi818@aucklanduni.ac.nz>
2024-04-08 21:30:36 +1200

    changed layout, logo and sizing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,33 +3,31 @@ import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 // import { useState } from 'react';
 // import Home from "@pages/Home";
 import Passport from "@pages/Passport";
-import GoogleSigninBtn from './components/GoogleSigninBtn.tsx';
+import SignInPage from './pages/SignIn-Page/SignInPage';
+import SignInErrorPage from "./pages/SigninError-Page/SignInErrorPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <p>Hello World!</p>
-        <Link to="passport">Other Page</Link>
-      </div>
-    ),
-  },
-  {
-    path: "/passport",
-    element: <Passport />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <div>
+//         <p>Hello World!</p>
+//         <Link to="passport">Other Page</Link>
+//       </div>
+//     ),
+//   },
+//   {
+//     path: "/passport",
+//     element: <Passport />,
+//   },
+// ]);
 
 export default function App() {
-  const handleClick = (): void => {
-    console.log('Custom Button was clicked!');
-  };
   
   return (
     <div>
-      <GoogleSigninBtn onClick={handleClick} />
-      <RouterProvider router={router} />;
+      <SignInPage />
+{/* <RouterProvider router={router} />; */}
     </div>
   );
 }
