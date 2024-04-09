faa47c3faefa38849783f4f1f334e4b9cdbe0864
bwon783 <86991521+BW82262@users.noreply.github.com>
2024-04-09 14:00:15 +1200

    Added in logic for sign check

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -3,10 +3,56 @@ import { JwtPayload, jwtDecode } from "jwt-decode"
 
 //getting rid of  error :D
 interface OathPayload extends JwtPayload {
-  family_name: string;
-  given_name: string;
-  email: string;
-  jti: string;
+  family_name: string
+  given_name: string
+  email: string
+  jti: string
+}
+
+interface UserData {
+  userData: OathPayload
+  UserUPI: string
+}
+
+// sending data to mongoDB, need to change url
+const postUserData = async (data: UserData) => {
+  await fetch("http://localhost:3000/api/v1/users", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({
+      firstName: data.userData.given_name,
+      lastName: data.userData.family_name,
+      email: data.userData.email,
+      accessToken: data.userData.jti,
+      upi: data.UserUPI,
+    }),
+  })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+const updateUserData = async (data: UserData) => {
+  await fetch("http://localhost:3000/api/v1/users", {
+    method: "PUT",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({
+      firstName: data.userData.given_name,
+      lastName: data.userData.family_name,
+      email: data.userData.email,
+      accessToken: data.userData.jti,
+      upi: data.UserUPI,
+    }),
+  })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 function Login() {
@@ -19,79 +65,97 @@ function Login() {
             credentialResponse.credential == null ||
             credentialResponse.credential == undefined
           ) {
-            throw new Error("Credential is null or undefined");
+            throw new Error("Credential is null or undefined")
           } else {
-            const jsondata : OathPayload = jwtDecode(credentialResponse.credential)
-            
+            const jsondata: OathPayload = jwtDecode(
+              credentialResponse.credential
+            )
+
+            console.log(jsondata)
+
             //extracting user UPI
-            const UserUPI = jsondata.email.split("@")[0];
+            const UserUPI = jsondata.email.split("@")[0]
 
             //passes UPI to WDCC member checker API
-            const checkUser = async (upi: any): Promise<String | undefined> => {
+            const checkUser = async (
+              upi: string
+            ): Promise<string | undefined> => {
               try {
-                const response = await fetch("https://membership.wdcc.co.nz/api/verify/sejJBBABIXoePtuQlPkbY/UPI/" + upi, {
-                  method: "GET"
-                });
-            
-                if (!response.ok) {
-                  throw new Error('Failed to connect to verification API');
-                }
-            
-                const text = await response.text();
-                //console.log(text);
-                return text;
+                const response = await fetch(
+                  "https://membership.wdcc.co.nz/api/verify/" +
+                    import.meta.env.VITE_MEMBERSHIP_CHECKER_SECRETS +
+                    "/UPI/" +
+                    upi,
+                  {
+                    method: "GET",
+                  }
+                )
 
+                if (!response.ok) {
+                  throw new Error("Failed to connect to verification API")
+                }
+
+                const text = await response.text()
+                //console.log(text);
+                return text
               } catch (error) {
-                console.error('Error verifying user:', error);
+                console.error("Error verifying user:", error)
               }
-            };
-            
+            }
+
             //passing userUPI to member checker
-            const text = await checkUser(UserUPI);
-          
+            const text = await checkUser(UserUPI)
+
             //checking is user email is in domain & if user is in WDCC
-            if (jsondata.email.endsWith("aucklanduni.ac.nz") && text == "value found in column"){
+            if (
+              jsondata.email.endsWith("aucklanduni.ac.nz") &&
+              text == "value found in column"
+            ) {
               console.log("YOU'RE IN WDCC!!")
 
               // Need to create a fork?, if a user is valid and is their first time logging in, then we would need to post
               //otherwise, need to update
 
-              // sending data to mongoDB, need to change url
-              const postUserData = async () => {
-                await fetch("http://localhost:3000/api/v1/users", {
-                  method: "POST",
-                  headers: { "Content-type": "application/json" },
-                  body: JSON.stringify({
-                    firstName: jsondata!.given_name,
-                    lastName: jsondata!.family_name,
-                    email: jsondata!.email,
-                    accessToken: jsondata!.jti,
-                  }),
+              const getUserData = async () => {
+                await fetch("http://localhost:3000/api/v1/users/" + UserUPI, {
+                  method: "GET",
                 })
                   .then((response) => {
                     console.log(response)
+                    // If we get something then, update the user data. Else post.
+                    if (response.status == 200) {
+                      updateUserData({
+                        userData: jsondata,
+                        UserUPI: UserUPI,
+                      }).then(() => {
+                        localStorage.setItem("accessToken", jsondata!.jti)
+                      })
+                    } else {
+                      postUserData({
+                        userData: jsondata,
+                        UserUPI: UserUPI,
+                      }).then(() => {
+                        localStorage.setItem("accessToken", jsondata!.jti)
+                      })
+                    }
                   })
                   .catch((error) => {
                     console.log(error)
                   })
-                  postUserData().then(() => {
-                    localStorage.setItem("accessToken", jsondata!.jti)
-                  })
               }
-
+              getUserData()
             } else {
               //redirect user to Error Page
               console.log("Send user to Error Page")
             }
           }
         }}
-
         onError={() => {
-          console.log("Login Failed");
+          console.log("Login Failed")
         }}
       />
     </div>
-  );
+  )
 }
 
 export default Login
