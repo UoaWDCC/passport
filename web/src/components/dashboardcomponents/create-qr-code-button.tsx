7ecefb772dda09f97e8e60682360e06709fc423f
CalebWharton <calebwharton123@gmail.com>
2024-04-09 12:00:24 +1200

    updated stamp file to take account of overflow

--- web/src/components/dashboardcomponents/create-qr-code-button.tsx
+++ web/src/components/dashboardcomponents/create-qr-code-button.tsx
@@ -2,9 +2,11 @@ import CreateNewButton from './create-new-button';
 
 function GenerateQRCodeButton() {
     return (
-        <div className='generate-qr-code-button'>
-            <h3 className='button-header'>QR Code Generator</h3>
-            <CreateNewButton />
+        <div className='qr-button-container'>
+            <div className='generate-qr-code-button'>
+                <h3 className='button-header'>QR Code Generator</h3>
+                <CreateNewButton />
+            </div>
         </div>
     );
 }
--- web/src/components/formcomponents/stamp-section.tsx
+++ web/src/components/formcomponents/stamp-section.tsx
@@ -5,13 +5,26 @@ import Bin from '../../assets/bin.svg';
 function StampSection() {
     const [stamp, setStamp] = useState('');
     const [fileSize, setFileSize] = useState('');
-    const HandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const [fileName, setFileName] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if(e.target.files) {
-            setFileSize((e.target.files[0].size / 1000).toFixed(2) + ' kb');
-            setStamp(e.target.value);
+            let stampSize = e.target.files[0].size
+            if (stampSize < 1000000) {
+                setFileSize((stampSize / 1000).toFixed(2) + ' kb');
+            } else {
+                setFileSize((stampSize / 1000000).toFixed(2) + ' mb');
+            }
+            let stampName = e.target.value;
+            let stampDisplayName = e.target.files[0].name;
+            setStamp(stampName);
+            if (stampDisplayName.length < 23) {
+                setFileName(stampDisplayName);
+            } else {
+                setFileName(stampDisplayName.substring(0,19) + '...');
+            }
         }
     }
-
 
     const handleUploadClick = () => {
         const stampElement = document.getElementById('stamp');
@@ -35,12 +48,12 @@ function StampSection() {
                 type='file' 
                 accept='.png'
                 value={stamp} 
-                onChange={HandleChange} />
+                onChange={handleChange} />
                 {!stamp && <p className='stamp-text'>select image to upload</p>}
-                {stamp && <p className='stamp-text'>{stamp.split("\\")[stamp.split("\\").length - 1]}</p>}
+                {stamp && <p className='stamp-text'>{fileName}</p>}
                 {stamp && <p className='stamp-text' id='file-size'>{fileSize}</p>}
                 {stamp && <img className="stamp-input-image" id="bin" src={Bin} alt="Bin" onClick={handleBinClick} />}
-                {!stamp && <img className="stamp-input-image" id='upload' src={Upload} alt="Upload" />}
+                {!stamp && <img className="stamp-input-image" id='upload' src={Upload} alt="Upload" onClick={handleUploadClick} />}
             </div>
         </div>
     )
