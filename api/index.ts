0a7219fdd89f83b7fc78771a4dcc2fdc5dd951cb
alden maiyor <amai111@aucklanduni.ac.nz>
2024-04-10 21:10:45 +1200

    Backend api working

--- api/index.ts
+++ api/index.ts
@@ -5,34 +5,20 @@ import { config } from "dotenv"
 
 // Import Routers
 import helloRoutes from './routes/hello';
-import apis from "./routes/Api"
+import apiRoutes from "./routes/Api"
 
-const app = express()
-config()
+const app = express();
+config();
 
-const databaseUrl: string = process.env.DATABASE_URL!
-
-connect(databaseUrl)
-  .then(() => {
-    console.log("Connected to MongoDB")
-    // Start the server after successful connection
-    const port = Number.parseInt(process.env.PORT || "3000")
-    app.listen(port, () => {
-      console.log(`Listening on port ${port}`)
-    })
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error)
-    process.exit(1) // Exit the process with a non-zero status code to indicate failure
-  })
-
-app.use(json())
-app.use(cors())
-app.use(express.static("public"))
+app.use(json());
+app.use(cors());
+app.use(express.static('public'));
 
 // Routes
 app.use('/hello', helloRoutes);
-app.use('/api', apis)
+apiRoutes.then((apiRouter) => {
+  app.use('/api', apiRouter)
+})
 
 const port = Number.parseInt(process.env.PORT || '3000');
 app.listen(port, () => {
