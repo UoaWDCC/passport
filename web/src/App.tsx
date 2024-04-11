5a01d0de5562861730e7a090b814dc3d5c4e5405
inezchong7 <inezchong7@gmail.com>
2024-04-11 16:37:43 +1200

    Revert "changed layout, logo and sizing"
    This reverts commit 87a117b43114aee0020cb882043e2d75bfa4d4d4.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,31 +3,33 @@ import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 // import { useState } from 'react';
 // import Home from "@pages/Home";
 import Passport from "@pages/Passport";
-import SignInPage from './pages/SignIn-Page/SignInPage';
-import SignInErrorPage from "./pages/SigninError-Page/SignInErrorPage";
+import GoogleSigninBtn from './components/GoogleSigninBtn.tsx';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <div>
-//         <p>Hello World!</p>
-//         <Link to="passport">Other Page</Link>
-//       </div>
-//     ),
-//   },
-//   {
-//     path: "/passport",
-//     element: <Passport />,
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <p>Hello World!</p>
+        <Link to="passport">Other Page</Link>
+      </div>
+    ),
+  },
+  {
+    path: "/passport",
+    element: <Passport />,
+  },
+]);
 
 export default function App() {
+  const handleClick = (): void => {
+    console.log('Custom Button was clicked!');
+  };
   
   return (
     <div>
-      <SignInPage />
-{/* <RouterProvider router={router} />; */}
+      <GoogleSigninBtn onClick={handleClick} />
+      <RouterProvider router={router} />;
     </div>
   );
 }
--- web/src/pages/SigninError-Page/SignInErrorPage.tsx
+++ web/src/pages/SigninError-Page/SignInErrorPage.tsx
@@ -1,17 +1,17 @@
-import styles from "./SignInError.module.css"; 
-import WDCC_Logo from "../../assets/WDCC_Logo.svg"
+import styles from "./SignInError.module.css"; // Import the CSS module
 
 const SignInErrorPage: React.FC = () => {
     return (
       <div className={styles.container}>
         <div className={styles.topsection}>
-          <img src={WDCC_Logo} alt="WDCC Logo" className={styles.logo} />
+        <h1 className={styles.h1}>WDCC</h1>
+          <p className={styles.subtitle}>Web Dev & Consulting Club</p>
         </div>
         
         <div className={styles.bottomsection}>
             <p className={styles.errorbig}>OOPS!</p>
             <p className={styles.errorsmall}>Looks like you're not registered with WDCC :(</p>
-          <a className={styles.link} href="https://forms.gle/YX8RAdXGF4rTN3e27">Register Here</a>
+          <a className={styles.text} href="https://forms.gle/YX8RAdXGF4rTN3e27">Register Here</a>
         </div>
       </div>
     );
