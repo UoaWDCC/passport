bf3d9ccab8553101cc50dfa23ab8aea9a9e417c6
CalebWharton <calebwharton123@gmail.com>
2024-04-12 14:41:46 +1200

    rebased to main

--- web/src/components/PassportMain.tsx
+++ web/src/components/PassportMain.tsx
@@ -1,7 +1,7 @@
 import Logo from "../assets/WDCC_Logo.svg";
 import "../styles/page styles/Passport.css";
 
-export default function PassportMain({}) {
+export default function PassportMain() {
   return (
     <div className={`passport passport-main m-auto rounded-br-3xl flex justify-center items-center `}>
       {/* <h1>This is the passport main page</h1> */}
