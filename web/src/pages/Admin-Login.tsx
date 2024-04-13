b49ceac4a2315e40c85068a6e0ce53649476b8c7
CalebWharton <calebwharton123@gmail.com>
2024-04-13 15:22:52 +1200

    renamed to not clash with other sheets

--- web/src/pages/Admin-Login.tsx
+++ web/src/pages/Admin-Login.tsx
@@ -7,9 +7,9 @@ function AdminLogin() {
     return (
         <div className="admin-login-outer background-admin-login">
             <div className="admin-login-left-items">
-                <h2 className="welcome-title">Welcome to the</h2>
-                <h1 className="passport-title">WDCC Passport</h1>
-                <h2 className="dashboard-title">Admin Dashboard</h2>
+                <h2 className="login-welcome-title">Welcome to the</h2>
+                <h1 className="login-passport-title">WDCC Passport</h1>
+                <h2 className="login-dashboard-title">Admin Dashboard</h2>
                 <GoogleSigninBtn onClick={() => {}} adminLogin={true} />
             </div>
             <div className="admin-login-right-items">
