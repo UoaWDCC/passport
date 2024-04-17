74b70c163a1ef81e9910733877e9f5ec88f72ffb
justin <jhua515@aucklanduni.ac.nz>
2024-04-17 22:41:36 +1200

    Implement call to get all events endpoint from events page

--- web/src/pages/Events.tsx
+++ web/src/pages/Events.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Events() {
+    const [events, setEvents] = useState();
+
+    useEffect(() => {
+        getEvents();
+    }, []);
+
+    const getEvents = async () => {
+        const eventsResponse = await axios.get(
+            "http://localhost:3000/api/get-all-events"
+        );
+        setEvents(eventsResponse.data);
+    };
+
     return (
         <div>
             <h1>Events</h1>
+            {events ? (
+                <ul>
+                    {events.map((event) => (
+                        <li key={event._id}>{event._id}</li>
+                    ))}
+                </ul>
+            ) : (
+                <h1>Loading</h1>
+            )}
         </div>
     );
 }
