673702b3879dea662f7ee76814f75451e5e2ef72
justin <jhua515@aucklanduni.ac.nz>
2024-04-17 16:14:21 +1200

    Create events page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,46 +1,51 @@
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import Login from "@pages/Login"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import { useState } from 'react';
 // import Home from "@pages/Home";
 import Passport from "@pages/Passport";
-import Dashboard from './pages/Dashboard.tsx';
-import Form from './pages/Form.tsx';
-import SignInPage from './pages/SignInPage.tsx';
+import Dashboard from "./pages/Dashboard.tsx";
+import Events from "@pages/Events.tsx";
+import Form from "./pages/Form.tsx";
+import SignInPage from "./pages/SignInPage.tsx";
 import SignInErrorPage from "./pages/SignInErrorPage.tsx";
 // import GoogleSigninBtn from "@components/GoogleSigninBtn";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-      <SignInPage />
-      </div>
-    ),
-  },
-  {
-    path: "/passport",
-    element: <Passport />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "/form",
-    element: <Form />,
-  },
-  {
-    path: "/sign-in-error",
-    element: <SignInErrorPage />,
-  },
+    {
+        path: "/",
+        element: (
+            <div>
+                <SignInPage />
+            </div>
+        ),
+    },
+    {
+        path: "/passport",
+        element: <Passport />,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+    },
+    {
+        path: "/dashboard/events",
+        element: <Events />,
+    },
+    {
+        path: "/form",
+        element: <Form />,
+    },
+    {
+        path: "/sign-in-error",
+        element: <SignInErrorPage />,
+    },
 ]);
 
 export default function App() {
-  return (
-    <div>
-      <RouterProvider router={router} />;
-    </div>
-  )
+    return (
+        <div>
+            <RouterProvider router={router} />;
+        </div>
+    );
 }
