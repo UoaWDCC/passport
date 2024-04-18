46fcc795848546ac21ac360efffb2d6e4bf45e99
justin <jhua515@aucklanduni.ac.nz>
2024-04-18 15:50:51 +1200

    Add styling to leaderboard page

--- web/src/pages/Leaderboard.tsx
+++ web/src/pages/Leaderboard.tsx
@@ -1,8 +1,9 @@
 import StampsAwayCount from "@components/StampsAwayCount";
+import "../styles/page styles/Leaderboard.css";
 
 export default function Leaderboard() {
     return (
-        <div>
+        <div className="passport-main h-screen">
             <h1>Leaderboard</h1>
             <StampsAwayCount />
         </div>
