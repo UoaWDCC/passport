7d515e8343eb360872bc21407fb365fda495dbda
justin <jhua515@aucklanduni.ac.nz>
2024-04-18 15:46:41 +1200

    Implement stamps away count component

--- web/src/pages/Leaderboard.tsx
+++ web/src/pages/Leaderboard.tsx
@@ -1,7 +1,10 @@
+import StampsAwayCount from "@components/StampsAwayCount";
+
 export default function Leaderboard() {
     return (
         <div>
             <h1>Leaderboard</h1>
+            <StampsAwayCount />
         </div>
     );
 }
