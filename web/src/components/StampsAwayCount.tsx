26d03cf49b24b9c4426486733c83a752a146c976
justin <jhua515@aucklanduni.ac.nz>
2024-04-18 15:50:51 +1200

    Add styling to leaderboard page

--- web/src/components/StampsAwayCount.tsx
+++ web/src/components/StampsAwayCount.tsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 
 export default function StampsAwayCount() {
     const [stampsAway, setStampsAway] = useState();
-    const temp = 1;
+    const temp = 2;
 
     return (
         <div className="text-center text-xl">
-            <p className="font-bold">{temp}</p>
+            <p className="font-bold text-2xl">{temp}</p>
             <div className="flex flex-col text-center">
                 <span>Stamps</span>
                 <span>away from</span>
                 <span>the next</span>
-                <span>prize</span>
+                <span>prize!</span>
             </div>
         </div>
     );
