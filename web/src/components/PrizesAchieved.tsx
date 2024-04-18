30402c3ec4e7aa0a901489381181ed8a377cf7f6
Erica <engi850@aucklanduni.ac.nz>
2024-04-18 15:58:20 +1200

    created present svg, and started on component for prizes achieved

---
+++ web/src/components/PrizesAchieved.tsx
@@ -0,0 +1,19 @@
+import Present from "../assets/present.svg";
+import "../styles/page styles/Leaderboard.css";
+
+
+const CustomComponent = () => {
+  return (
+    <div className="container">
+      <div className="text">
+        height x
+      </div>
+      <Present />
+      <div className="text">
+        prizes achieved
+      </div>
+    </div>
+  );
+}
+
+export default CustomComponent;
