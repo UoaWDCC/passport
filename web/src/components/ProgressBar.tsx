96d396f17a1071ba41df9bc55c452e7e08534a30
justin <jhua515@aucklanduni.ac.nz>
2024-04-18 16:18:05 +1200

    Add progress bar with dynamic height

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Man from "../assets/man.svg";
 
 export default function ProgressBar() {
+    const [height, setHeight] = useState(3);
+
     return (
-        <div>
+        <div className="flex">
             <img src={Man} alt="" />
+            <div
+                className="bg-blue-500 w-10"
+                style={{ height: `${height * 50}px` }}
+            ></div>
         </div>
     );
 }
