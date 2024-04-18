e740a1d866eaff8652e473e17c6858c73b3cbe76
justin <jhua515@aucklanduni.ac.nz>
2024-04-18 15:46:41 +1200

    Implement stamps away count component

---
+++ web/src/components/StampsAwayCount.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function StampsAwayCount() {
+    const [stampsAway, setStampsAway] = useState();
+    const temp = 1;
+
+    return (
+        <div className="text-center text-xl">
+            <p className="font-bold">{temp}</p>
+            <div className="flex flex-col text-center">
+                <span>Stamps</span>
+                <span>away from</span>
+                <span>the next</span>
+                <span>prize</span>
+            </div>
+        </div>
+    );
+}
