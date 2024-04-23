50be155f914528b7503e242224909f10f62c9af2
justin <jhua515@aucklanduni.ac.nz>
2024-04-23 15:10:17 +1200

    Display qr code and stamp image on dashboard

--- web/src/pages/Events.tsx
+++ web/src/pages/Events.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/page styles/event.css"
-import logo from '../assets/primary_logo.svg'
+import "../styles/page styles/event.css";
+import logo from "../assets/primary_logo.svg";
 
 export default function Events() {
     const [events, setEvents] = useState();
@@ -18,14 +18,14 @@ export default function Events() {
     };
 
     return (
-        <div >
+        <div>
             <img src={logo} alt="Logo" className="logo" />
             <h1 className="title">Event Dashboard</h1>
 
             <div className="dashboard">
-            <a href="/form" className="create-event-button">
-                Create new event
-            </a>
+                <a href="/form" className="create-event-button">
+                    Create new event
+                </a>
                 <div className="dashboard-header">
                     <div className="column">Name</div>
                     <div className="column">QR Code</div>
@@ -39,10 +39,28 @@ export default function Events() {
                         {events.map((event) => (
                             <li key={event._id} className="event-item">
                                 <div className="column">{event.eventName}</div>
-                                <div className="column">{event.qrCode}</div>
-                                <div className="column">{event.stampImage}</div>
+                                <div className="column">
+                                    <img
+                                        src={event.QRcode}
+                                        alt=""
+                                        className="w-20"
+                                    />
+                                </div>
+
+                                <div className="column">
+                                    <img
+                                        src={event.stamp64}
+                                        alt=""
+                                        className="w-20"
+                                    />
+                                </div>
+
                                 <div className="column">{event.status}</div>
-                                <div className="column">{Array.isArray(event.attendees) ? event.attendees.length : 0}</div> 
+                                <div className="column">
+                                    {Array.isArray(event.attendees)
+                                        ? event.attendees.length
+                                        : 0}
+                                </div>
                                 <div className="column">
                                     <button>Edit</button>
                                     <button>Delete</button>
