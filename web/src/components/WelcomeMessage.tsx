9ea701c5647058b9ea3913256e12ffaaaafbd8d6
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2024-04-23 15:21:45 +1200

    merged branch with main

---
+++ web/src/components/WelcomeMessage.tsx
@@ -0,0 +1,7 @@
+export default function WelcomeMessage() {
+    return (
+    <div className="pt-3 text-left flex item-start">
+        <h1 className="text-2xl text-blue-950"><span className="italic">Welcome</span> <span className="font-semibold">Inez</span></h1>
+    </div>
+    );
+}
---
+++ web/src/pages/Admin-Login.tsx
@@ -0,0 +1,23 @@
+import GoogleSigninBtn from "@components/GoogleSigninBtn";
+import PassportMain from "@components/PassportMain";
+import "../styles/page styles/Admin-Login.css";
+import useGoogleSignIn from "./Login";// Import the custom hook
+
+function AdminLogin() {
+    const handleSignIn = useGoogleSignIn();
+    return (
+        <div className="admin-login-outer background-admin-login">
+            <div className="admin-login-left-items">
+                <h2 className="welcome-title-admin-login">Welcome to the</h2>
+                <h1 className="passport-title-admin-login">WDCC Passport</h1>
+                <h2 className="dashboard-title-admin-login">Admin Dashboard</h2>
+                <GoogleSigninBtn onClick={handleSignIn} adminLogin={true} />
+            </div>
+            <div className="admin-login-right-items">
+                <PassportMain />
+            </div>
+        </div>
+    );
+}
+
+export default AdminLogin;
