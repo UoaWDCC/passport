f78d229ecd38fff834eab7d0310b526298b02b5e
Erica <engi850@aucklanduni.ac.nz>
2024-05-01 22:18:12 +1200

    overlay attempted for progress bar, and spacing of components

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import Man from "../assets/man.svg";
 import manAndBar from "../assets/manAndBar.svg";
+import barLines from "../assets/barLines.svg";
 
 export default function ProgressBar() {
     const [height, setHeight] = useState(3);
 
     return (
-        <div className="flex">
+        <div className="relative">
             <img src={manAndBar} alt="" />
             <div
-                className="bg-blue-500 w-10"
-                style={{ height: `${height * 50}px` }}
+                className="absolute"
+                style={{ 
+                    backgroundColor: '#097DF1',
+                    bottom: '16px', // Adjust this value
+                    left: '96px', // Adjust this value
+                    width: '36px', // Adjust this value
+                    height: `${height * 50}px` 
+                }}
             ></div>
+            <img src={barLines} alt="" className="absolute" style={{ left: '119px', bottom: '50px' }} />
         </div>
     );
 }
