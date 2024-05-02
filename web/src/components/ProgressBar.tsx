2b9c1308f0be17b6ad3eee1f100839f5eb029803
Erica <engi850@aucklanduni.ac.nz>
2024-05-02 15:44:42 +1200

    renamed files fixed bar height

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
-import Man from "../assets/man.svg";
-import manAndBar from "../assets/manAndBar.svg";
+import manAndBar from "../assets/presentManAndBar.svg";
 import barLines from "../assets/barLines.svg";
 
 export default function ProgressBar() {
-    const [height, setHeight] = useState(3);
+  const [height, setHeight] = useState(3);
 
-    return (
-        <div className="relative">
-            <img src={manAndBar} alt="" />
-            <div
-                className="absolute"
-                style={{ 
-                    backgroundColor: '#097DF1',
-                    bottom: '16px', // Adjust this value
-                    left: '96px', // Adjust this value
-                    width: '36px', // Adjust this value
-                    height: `${height * 50}px` 
-                }}
-            ></div>
-            <img src={barLines} alt="" className="absolute" style={{ left: '119px', bottom: '50px' }} />
-        </div>
-    );
+  return (
+    <div className="relative">
+      <img src={manAndBar} alt="" />
+      <div
+        className="absolute"
+        style={{
+          backgroundColor: "#097DF1",
+          bottom: "16px", // Adjust this value
+          left: "96px", // Adjust this value
+          width: "36px", // Adjust this value
+          height: `${height * 108}px`, // max: 540px (108 per height unit)
+        }}
+      ></div>
+      <img
+        src={barLines}
+        alt=""
+        className="absolute"
+        style={{ left: "119px", bottom: "50px" }}
+      />
+    </div>
+  );
 }
--- web/src/components/StampsAwayCount.tsx
+++ web/src/components/StampsAwayCount.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 
 export default function StampsAwayCount() {
-    const [stampsAway, setStampsAway] = useState();
-    const temp = 2;
+  const [stampsAway, setStampsAway] = useState(2);
 
-    return (
-        <div className="text-center text-xl">
-            <p className="font-semibold text-4xl">{temp}</p>
-            <div className="flex flex-col text-center">
-                <span>Stamps</span>
-                <span>away from</span>
-                <span>the next</span>
-                <span>prize!</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className="text-center text-xl">
+      <p className="font-semibold text-4xl">{stampsAway}</p>
+      <div className="flex flex-col text-center">
+        <span>Stamps</span>
+        <span>away from</span>
+        <span>the next</span>
+        <span>prize!</span>
+      </div>
+    </div>
+  );
 }
