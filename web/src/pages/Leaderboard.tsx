4c1c340c4c82ad7b5313672ee2ad635f14b14518
CalebWharton <calebwharton123@gmail.com>
2024-05-12 12:32:41 +1200

    Added CheckLoggedIn to new relevant pages

--- web/src/pages/Leaderboard.tsx
+++ web/src/pages/Leaderboard.tsx
@@ -5,27 +5,30 @@ import ProgressBar from "@components/ProgressBar";
 import RedeemPrizeButton from "@components/RedeemPrizeButton";
 import "../styles/page styles/Leaderboard.css";
 import PrizesAchieved from "@components/PrizesAchieved";
+import CheckLoggedIn from "@components/CheckLoggedIn";
 
 export default function Leaderboard() {
   const [height] = useState(5);
 
   return (
-    <div className="leaderboard-main h-screen flex flex-col items-center justify-center">
-      <div className="flex items-center space-x-4">
-        <ProgressBar height={height} />
-        <StampsAwayCount height={height} />
-      </div>
-      {height >= 5 ? (
-      <Link to="/leaderboard-prize">
-        <button className="mt-6">
-            <RedeemPrizeButton />
-          </button>
-      </Link>
-    ) : (
-      <div className="mt-6">
-      <PrizesAchieved/>
-      </div>
-    )}
-  </div>
+    <CheckLoggedIn>
+      <div className="leaderboard-main h-screen flex flex-col items-center justify-center">
+        <div className="flex items-center space-x-4">
+          <ProgressBar height={height} />
+          <StampsAwayCount height={height} />
+        </div>
+        {height >= 5 ? (
+        <Link to="/leaderboard-prize">
+          <button className="mt-6">
+              <RedeemPrizeButton />
+            </button>
+        </Link>
+      ) : (
+        <div className="mt-6">
+        <PrizesAchieved/>
+        </div>
+      )}
+    </div>
+  </CheckLoggedIn>
   );
 }
