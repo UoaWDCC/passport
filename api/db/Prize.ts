7866dcac2b81305d503092849f6a48b09d96de6c
bwon783 <86991521+BW82262@users.noreply.github.com>
2024-07-02 22:05:17 +1200

    Updated Prize Data Model

--- api/db/Prize.ts
+++ api/db/Prize.ts
@@ -4,6 +4,8 @@ const prizeSchema = new Schema({
   userId: String,
   redeemed: Boolean,
   redeemedTime: Date,
+  firstName: String,
+  lastName: String,
 })
 
 const Prize = model("Prize", prizeSchema, "Prizes")
