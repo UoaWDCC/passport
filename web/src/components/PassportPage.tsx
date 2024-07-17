ea782d65b88ac1cedf4d6f5070991f68abd24d59
justin <jhua515@aucklanduni.ac.nz>
2024-07-18 00:31:35 +1200

    Add key to stamp divs

--- web/src/components/PassportPage.tsx
+++ web/src/components/PassportPage.tsx
@@ -1,16 +1,19 @@
 import "../styles/page styles/Passport.css";
 
-export default function PassportPage({stamps}) {
-  return (
-    <div className="passport bg-white m-auto rounded-br-3xl">
-      <div className="grid grid-cols-2 gap-4">
-        {stamps.map((stamp, index) => (
-          <div>
-            <img className="stamp-image" src={stamp.stamp64} alt={`Stamp $${index+1}`}></img>
-          </div>
-        ))}
-
-      </div>
-    </div>
-  );
+export default function PassportPage({ stamps }) {
+    return (
+        <div className="passport bg-white m-auto rounded-br-3xl">
+            <div className="grid grid-cols-2 gap-4">
+                {stamps.map((stamp, index) => (
+                    <div key={index}>
+                        <img
+                            className="stamp-image"
+                            src={stamp.stamp64}
+                            alt={`Stamp $${index + 1}`}
+                        ></img>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
