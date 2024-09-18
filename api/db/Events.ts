7d12a50b33a2abd0203898e98add8f0b6b612cc0
justin <jhua515@aucklanduni.ac.nz>
2024-09-18 17:56:41 +1200

    add event venue and description fields

--- api/db/Events.ts
+++ api/db/Events.ts
@@ -1,14 +1,16 @@
-import { Schema, model } from "mongoose"
+import { Schema, model } from "mongoose";
 
 const EventsSchema = new Schema({
-   eventName: {type: String, req: true},
-   stamp64: {type: String, req: true},
-   startDate:{type:Date, req:true},
-   endDate: {type: Date, req:true},
-   totalAttended: {type: Number, default: 0, req:true},
-   QRcode: {type: String, req:true}
-})
+    eventName: { type: String, req: true },
+    stamp64: { type: String, req: true },
+    startDate: { type: Date, req: true },
+    endDate: { type: Date, req: true },
+    eventVenue: { type: String, req: true },
+    eventDescription: { type: String, req: true },
+    totalAttended: { type: Number, default: 0, req: true },
+    QRcode: { type: String, req: true },
+});
 
-const Events = model("Event", EventsSchema)
+const Events = model("Event", EventsSchema);
 
-export default Events
+export default Events;
