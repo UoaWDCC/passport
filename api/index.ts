6b9d8f7891d4de3f1eb6f8cad89dd40617373f01
Nate Williamson <121474948+Oculux314@users.noreply.github.com>
2025-03-25 14:36:54 +1300

    Try enabling CORS for passport.wdcc.co.nz

--- api/index.ts
+++ api/index.ts
@@ -17,7 +17,9 @@ import eventsRoute from "./routes/events";
 const app = express();
 
 app.use(express.json({ limit: "500mb" }));
-app.use(cors());
+app.use(cors({
+  origin: "https://passport.wdcc.co.nz"
+}));
 app.use(express.static("public"));
 
 // Database connection string
